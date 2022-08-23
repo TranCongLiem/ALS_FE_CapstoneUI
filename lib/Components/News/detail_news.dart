cab207f49911abc63b28f650558688d32410f2a6
Tran Cong Liem <liemtc16@gmail.com>
2022-08-23 16:54:38 +0700

    Voice_To_Text design screen and code some part

---
+++ lib/Components/News/detail_news.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DetailNews extends StatefulWidget {
+  const DetailNews({Key? key}) : super(key: key);
+
+  @override
+  State<DetailNews> createState() => _DetailNewsState();
+}
+
+class _DetailNewsState extends State<DetailNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/Feature/SpeechToText/SpeechToText.dart
+++ lib/Feature/SpeechToText/SpeechToText.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:highlight_text/highlight_text.dart';
 import 'package:permission_handler/permission_handler.dart';
-
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 class SpeechToText extends StatefulWidget {
   const SpeechToText({Key? key}) : super(key: key);
 
@@ -10,31 +12,108 @@ class SpeechToText extends StatefulWidget {
 
 class _SpeechToTextState extends State<SpeechToText> {
 
+  final Map<String, HighlightedWord> _highlights = {
+
+
+  };
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Nhấn nút và bắt đầu nói';
+  double _confidence = 1.0;
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Giọng nói thành văn bản'),
+    //   ),
+    //   floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: () async {
+    //       PermissionStatus microStatus= await Permission.microphone.request();
+    //       if(microStatus == PermissionStatus.granted){
+    //
+    //       }
+    //       if(microStatus == PermissionStatus.denied){
+    //         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('This permission is recommend')));
+    //       }
+    //       if(microStatus == PermissionStatus.permanentlyDenied){
+    //         openAppSettings();
+    //       }
+    //     },
+    //     child: Icon(Icons.keyboard_voice),
+    //
+    //   ),
+    //
+    //
+    // );
     return Scaffold(
       appBar: AppBar(
-        title: Text('Giọng nói thành văn bản'),
+        title: Text('Đã nhận diện: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          PermissionStatus microStatus= await Permission.microphone.request();
-          if(microStatus == PermissionStatus.granted){
-
-          }
-          if(microStatus == PermissionStatus.denied){
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('This permission is recommend')));
-          }
-          if(microStatus == PermissionStatus.permanentlyDenied){
-            openAppSettings();
-          }
-        },
-        child: Icon(Icons.keyboard_voice),
-
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: false,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none, size: 50,),
+        ),
       ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: TextHighlight(
+            text: _text,
+            words: _highlights,
+            maxLines: 10,
+            textStyle: const TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w200,
 
-
+            ),
+          ),
+        ),
+      ),
     );
   }
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }
+          ),
+        );
+
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
 }
+
+
+
+
+
