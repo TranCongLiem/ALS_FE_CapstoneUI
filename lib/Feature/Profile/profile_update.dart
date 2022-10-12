0fecad87b61aa90d6405ab5e7b1f037957742e3b
Tran Cong Liem <liemtc16@gmail.com>
2022-10-12 21:37:57 +0700

    UI_SessionExercise

--- lib/Feature/Profile/profile_update.dart
+++ lib/Feature/Profile/profile_update.dart
@@ -1,4 +1,3 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:capstone_ui/Constant/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
