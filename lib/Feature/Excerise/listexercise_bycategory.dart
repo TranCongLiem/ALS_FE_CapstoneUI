92efb05c261f8a7e7e0da934fd6c6f85cf18e435
Tran Cong Liem <liemtc16@gmail.com>
2022-10-17 10:21:16 +0700

    ListExByCategory_UIChange

--- lib/Feature/Excerise/listexercise_bycategory.dart
+++ lib/Feature/Excerise/listexercise_bycategory.dart
@@ -16,86 +16,134 @@ class _ListExerciseByCategoryState extends State<ListExerciseByCategory> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Danh sách bài tập'),
+        elevation: 0.0,
+        backgroundColor: greenALS,
       ),
       body: Container(
-        color: Colors.green[50],
-        child: ListView.builder(
-          itemCount: 10,
-          shrinkWrap: true,
-          itemBuilder: (context, index) => Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            child: Card(
-              elevation: 3.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0.0),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                'Danh sách bài tập tay',
+                style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.grey),
               ),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  child: Card(
+                    elevation: 3.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.0),
+                    ),
+                    child: Column(
+                      children: [
                         Container(
-                          width: 110.0,
-                          height: 110.0,
-                          child: CircleAvatar(
-                            backgroundColor: Colors.green,
-                            foregroundColor: Colors.green,
-                            backgroundImage: NetworkImage(
-                                "https://bloganchoi.com/wp-content/uploads/2018/09/bai-tap-ta-tay.jpg"),
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    width: 110.0,
+                                    height: 110.0,
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.green,
+                                      foregroundColor: Colors.green,
+                                      backgroundImage: NetworkImage(
+                                          "https://bloganchoi.com/wp-content/uploads/2018/09/bai-tap-ta-tay.jpg"),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10.0,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        'Bài tập 1',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 24.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(
+                                        height: 5.0,
+                                      ),
+                                      // Text(
+                                      //   'Mức độ dễ',
+                                      //   style: TextStyle(color: Colors.grey),
+                                      // ),
+                                      TextButton(
+                                          style: TextButton.styleFrom(
+                                              foregroundColor: Colors.white,
+                                              backgroundColor: Colors.red[400]),
+                                          onPressed: () {},
+                                          child: Text(
+                                            'Khó',
+                                            style: TextStyle(fontSize: 20.0),
+                                          ))
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10.0, vertical: 10.0),
+                                child: ElevatedButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      "Thêm",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20.0),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: greenALS,
+                                        padding: EdgeInsets.all(15.0),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0)))),
+                              )
+                            ],
                           ),
                         ),
-                        SizedBox(
-                          width: 10.0,
+                        Container(
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            'Đây là bài tập kinh điển và đã được chứng minh là cực kỳ hiệu quả qua hàng thế hệ. Đây là bài tập không thể thiếu khi tập tay trước',
+                            maxLines: 5,
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Bài tập 1',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Text(
-                              'Mức độ dễ',
-                              style: TextStyle(color: Colors.grey),
-                            )
-                          ],
-                        )
                       ],
                     ),
-                    Container(
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 10.0),
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Thêm",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: greenALS,
-                              padding: EdgeInsets.all(20.0),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0)))),
-                    )
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
